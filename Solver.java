bc7f8f091173dafcfb09189f6b9f388d884b02ec
Laszlo Nagy <laszlo.nagy@student.manchester.ac.uk>
2018-02-16 23:46:28 +0000

    implemented function that opens doors

--- Solver.java
+++ Solver.java
@@ -5,30 +5,84 @@ import java.util.Arrays;
 
 public class Solver
 {
+  //allows generating random numbers
+  private static Random rand = new Random();
+
+  //number of doors in the game
+  private static int noOfDoors;
+
+  //number of closed doors
+  private static int closedDoors;
+
+  //array representing the doors
+  private static char doors[];
+
   public static void main(String args[])
   {
-    //local variables for keeping track of wins and loses
+    //variables for keeping track of wins and loses
     int wins = 0;
     int loses = 0;
 
-    //allows generating random numbers
-    Random rand = new Random();
-
     //number of doors in the game
-    int noOfDoors = 3;
+    noOfDoors = 5;
 
-    //array representing the doors
-    char doors[] = new char[noOfDoors];
+    //initially all doors are closed
+    closedDoors = noOfDoors;
 
+    //make array
+    doors = new char[noOfDoors];
 
-    /*repeat routine enough times for results to be representetive of
+    /*repeat game enough times for results to be representetive of
       probabilities*/
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 1; i++)
     {
       //initialise array with one car and the rest goats
       Arrays.fill(doors, 'G');
       doors[rand.nextInt(noOfDoors)] = 'C';
 
+      //randomly pick a door
+      int pickedDoor = rand.nextInt(noOfDoors - 1);
+
+      //code for testing array
+      /*for (char c : doors)
+        System.out.print(c);
+
+      System.out.println();*/
+
+      //keep opening doors until there are only 2
+      while (closedDoors > 2)
+      {
+        for (char c : doors)
+          System.out.print(c);
+
+        System.out.println();
+        openDoor(pickedDoor);
+      }
+      for (char c : doors)
+        System.out.print(c);
+
+      System.out.println();
+
     }//for
   }//main
+
+  //method representing the opening of a door with a goat behind it
+  private static void openDoor(int pickedDoor)
+  {
+    //variable stores which door will be opened
+    int doorToOpen;
+
+    //find a random goat door that isn't picked
+    do
+    {
+      doorToOpen = rand.nextInt(noOfDoors - 1);
+    } while (doors[doorToOpen] == 'C' || doors[doorToOpen] == pickedDoor
+             || doors[doorToOpen] == 'O');
+
+    //"open the door" by setting it's value to 'O' in the array
+    doors[doorToOpen] = 'O';
+
+    //update the amount of closed doors
+    closedDoors--;
+  }//openDoor
 }//class
